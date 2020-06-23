b50b6b1e3019e6977e55660c7a2da8f4cb6cc98a
passionary <jet.aldik@gmail.com>
2020-06-23 14:26:35 -0400

    error and message notifying changed

--- client/src/components/VideoCreater.vue
+++ client/src/components/VideoCreater.vue
@@ -3,6 +3,9 @@
     <div class="green lighten-3" v-if="nmessage">
       <p class="center-align white-text">{{nmessage}}</p>
     </div>
+    <div class="center-align materialize-red lighten-3" v-if="$store.state.error">
+      <span class="white-text">{{error}}</span>
+    </div>
     <input type="text" name="item[name]" class="form-control" placeholder="name" v-model="name">
     <form id="form" enctype="multipart/form-data" action="http://127.0.0.1:8000/api/request" method="post" class="form-group mx-auto" @submit.prevent="submitHandler">
       <input type="hidden" name="request[type]" value="video">
@@ -10,7 +13,7 @@
       </textarea>
       <input type="text" name="request[author]" class="form-control" placeholder="author" v-model="author">
       <input type="hidden" name="item[name]" class="form-control" placeholder="name" :value="name">
-      <input type="hidden" name="video_id" :value="$route.params.video && $route.params.video.video.id">
+      <input type="hidden" name="video_id" :value="$route.params.video && $route.params.video.video.id || created[0] && created[1] || ''" :key="created[0]">
       <input type="hidden" name="user_id" :value="user.id">
       <input type="hidden" name="request[email]" class="form-control" placeholder="name" v-model="user.email">
       <input type="hidden" name="MAX_FILE_SIZE" value="3000000000" />          
@@ -41,29 +44,38 @@
 </style>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 
 export default {
   data:() => ({    
     name:'',
     message:'',
-    nmessage:'',
+    created: [],
     author:'',
     tags:''
   }),
   methods: {
     ...mapActions(['auth']),
+    ...mapMutations(['setError','setMessage']),
     submitHandler() {
-      axios.post('http://127.0.0.1:8000/api/request',new FormData(form))
+      fetch('http://127.0.0.1:8000/api/request',{
+        method: "POST",
+        body: new FormData(form)
+      })
+      .then(res => res.json())
       .then(res => {
-        console.log(res)
-        this.nmessage = 'your video sended successfully'
-        localStorage.setItem(`type=video,name=${this.name}`,res.data.video)
+        if(res.errors || res.errors2) {
+          this.setError(Object.values(res.errors)[0] && Object.values(res.errors)[0][0] || Object.values(res.errors2)[0] && Object.values(res.errors2)[0][0])
+          return
+        }
+        this.setMessage(res.message || "")
+        this.created[0] = true
+        this.created[1] = res.id
       })
     }
   },
   computed: {
-    ...mapGetters(['user'])
+    ...mapGetters(['user','error','nmessage'])
   },
   async mounted() {
     await this.auth()
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,18 +7,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  	user:''
+  	user:'',
+    error:'',
+    nmessage:''
   },
   mutations: {
+    setError(state, mes) {
+      if(!mes) return
+      state.error = mes;
+      setTimeout(() => {
+        this.state.error = ''
+      },3000)
+    },
+    setMessage(state,mes) {
+      if(!mes) return
+      state.nmessage = mes
+      setTimeout(() => {
+        this.state.nmessage = ''
+      },3000)
+    }
   },
   actions: {
   	async auth() {
   		const req = await axios.get(`http://127.0.0.1:8000/api/user?token=${getCookie('token')}`)
   		this.state.user = req.data
-  	}
+  	},    
   },
   getters: {
-  	user: s => s.user
+  	user: s => s.user,
+    error: s => s.error,
+    nmessage: s => s.nmessage
   },
   modules: {
   }
